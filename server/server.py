614cde61909238bf561fa63ff734e562fc858d83
Lyna Hocine <lyna.hocine@etu.sorbonne-universite.fr>
2026-02-20 10:43:24 +0100

    Project structure

---
+++ server/server.py
@@ -0,0 +1,4 @@
+def web_search_endpoint():
+    """exposes the tool, receives JSON requests
+    calls the tool and returns results"""
+    return
---
+++ tool/web_search_tool.py
@@ -0,0 +1,3 @@
+def web_search_tool(query):
+    """the core tool, executes the web search"""
+    return
