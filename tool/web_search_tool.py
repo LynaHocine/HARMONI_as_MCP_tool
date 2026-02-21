af10a549b9da8bed707c040037b05b4d07fe7715
Lyna Hocine <lyna.hocine@etu.sorbonne-universite.fr>
2026-02-21 15:00:29 +0100

    web search tool with DuckDuckGo

--- tool/web_search_tool.py
+++ tool/web_search_tool.py
@@ -1,3 +1,6 @@
+import requests
+from bs4 import BeautifulSoup
+
 def web_search_tool(query):
     """It takes the user query (text) as input.
     Outputs result of the search 
@@ -6,7 +9,28 @@ def web_search_tool(query):
     q = query.strip()
     if not q: 
         return {"query": q, "result": "The query is empty"}
-    else: 
-        result = "web search"
+    
+    #web tool does search with DuckDuckGo
 
-        return {"query":q, "result":result}
+    url = f"https://html.duckduckgo.com/html/?q={q}"
+    res = requests.get(url)
+    
+    #extract the data from the html
+    soup = BeautifulSoup(res.text, "html.parser")
+
+    #get the top result 
+    results = soup.find_all("div", class_="result")
+
+    if not results: 
+        return {"query":q, "result": "No results found"}
+    
+    top = results[0]
+    title = (top.find("a", class_="result__a")).get_text()
+
+    snippet_tag = top.find("a", class_="result__snippet") or top.find("div", class_="result__snippet")
+    snippet = snippet_tag.get_text() if snippet_tag else "No snippet available"
+
+    result = f"{title}\n{snippet}"
+
+
+    return {"query":q, "result":result}
