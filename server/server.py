fa5dec6eed92f55fc5c535935b40f075691fe56c
mariduartef <mariduarteferreira@gmail.com>
2026-02-22 23:49:52 +0100

    server.py and uv init

--- server/server.py
+++ server/server.py
@@ -1,4 +1,32 @@
-def web_search_endpoint():
-    """exposes the tool, receives JSON requests
-    calls the tool and returns results"""
-    return
+from mcp.server.fastmcp import FastMCP
+from tool.web_search_tool import web_search_tool
+
+mcp = FastMCP("web-search") #create mcp server
+
+@mcp.tool()
+def call_web_search_tool(query : str) -> str : 
+    """
+    calls the web search tool and returns a response
+
+    Args : 
+        query (str) : sentence in natural language for the search tool
+    
+    Returns :
+        str : the response collected in the web
+    """
+    response_dic = web_search_tool(query)
+    return response_dic["result"]
+
+
+
+
+def main():
+    mcp.run()
+    #we can also use 
+    #mcp.run(mode="http", host="127.0.0.1", port=8000)
+    #if we want to use http and if is only going to run locally
+
+
+if __name__ == "__main__":
+    print("starting server")
+    main()
