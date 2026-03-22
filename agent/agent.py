e46a8531c1b1de2e67d9544cb077d593b0baca51
Lyna Hocine <lyna.hocine@etu.sorbonne-universite.fr>
2026-03-22 12:34:05 +0100

    Using vision model as a tool

--- agent/agent.py
+++ agent/agent.py
@@ -1,9 +1,7 @@
 import asyncio
-import re
 import sys
 import os
 import base64
-import requests
 import time
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 
@@ -12,24 +10,15 @@ BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 SERVER_DIR = os.path.join(BASE_DIR, "server")
 SERVER_FILE = os.path.join(SERVER_DIR, "server.py")
 
-from preprocessor.image_processor import ImagePreprocessor
 from preprocessor.speech_processor import SpeechToText
 from langchain_ollama import ChatOllama
 from langchain_core.messages import HumanMessage, AIMessage, ToolMessage
 from langchain_mcp_adapters.client import MultiServerMCPClient
 from langgraph.prebuilt.chat_agent_executor import create_react_agent
 
-vision_model = ChatOllama(model="llama3.2-vision", temperature=0)
 agent_model = ChatOllama(model="gpt-oss:20b", temperature=0)
 
-async def describe_image(data_uri: str, question:str) -> str:
-    """ask the vision model to describe the image."""
-    message = HumanMessage(content=[
-        {"type": "text", "text": f"Look at this image and describe it in the context of this question {question}"},
-        {"type": "image_url", "image_url": data_uri},
-    ])
-    response = await vision_model.ainvoke([message])
-    return response.content
+
 async def run_agent():
 
     client = MultiServerMCPClient({
@@ -53,30 +42,8 @@ async def run_agent():
             user_input = input()
         
         total_timer = time.perf_counter()
-        #searching for image url in the message
-        image_url_match = re.search(r'https?://\S+\.(?:png|jpg|jpeg)', user_input)
 
-        if image_url_match:
-            image_url = image_url_match.group()
-            #remove the url from the text to keep only the question
-            text = user_input.replace(image_url,"").strip()
-            data_uri = ImagePreprocessor.image_to_base64(image_url)
-
-            print("[Vision model analyzing image...]")
-            vision_timer = time.perf_counter()
-            image_description = await describe_image(data_uri, text)
-            vision_elapsed = time.perf_counter() - vision_timer
-            print(f"[Image description: {image_description}]")
-            print(f"[Vision timer = {vision_elapsed:.2f}s]")
-
-            combined = (
-                f"The user asked: {text}\n\n"
-                f"A vision model analyzed the image and said: {image_description}"
-                f"Using this context, answer user's question. "
-            )
-            message = HumanMessage(content=combined)
-        else: 
-            message = HumanMessage(content = user_input)
+        message = HumanMessage(content = user_input)
 
         agent_timer = time.perf_counter()
         response = await agent.ainvoke({
@@ -84,27 +51,27 @@ async def run_agent():
         })
         agent_elapsed = time.perf_counter() - agent_timer
 
-        for message in response["messages"]:
+        for msg in response["messages"]:
 
-            if isinstance(message, HumanMessage):
-                print("USER:", message.content)
+            if isinstance(msg, HumanMessage):
+                print("USER:", msg.content)
 
-            elif isinstance(message, AIMessage):
-                print("AI:", message.content)
+            elif isinstance(msg, AIMessage):
+                print("AI:", msg.content)
 
-                if message.tool_calls:
+                if msg.tool_calls:
                     print("TOOL CALL DETECTED:")
-                    for tool in message.tool_calls:
+                    for tool in msg.tool_calls:
                         print(tool)
                 
-            elif isinstance(message, ToolMessage):
-                print("TOOL RESPONSE:", message.content)
+            elif isinstance(msg, ToolMessage):
+                print("TOOL RESPONSE:", msg.content)
                 
             print()
 
         total_elapsed = time.perf_counter() - total_timer
-        print(f"[Agent time: {agent_elapsed:.2f}]s") 
-        print(f"[Total: {total_elapsed:.2f}]s")
+        print(f"[Agent time: {agent_elapsed:.2f}s]") 
+        print(f"[Total: {total_elapsed:.2f}s]")
         print()
 
     
